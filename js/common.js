e863b2ec76d8f4ac6a12cc977e76d171774b615c
leebom21 <52150668+leebom21@users.noreply.github.com>
2024-04-07 17:30:34 +0900

    feat:js 추가 및 스타일 정리

--- js/common.js
+++ js/common.js
@@ -1,25 +1,56 @@
 $(function () {
+  $("#testModal").hide();
+
+  //드롭다운 이벤트
   $("#drop").click(function () {
     $(".hide_area").toggleClass("hide");
     $(this).toggleClass("icon_rotate");
   });
 
-  $(".tip_icon, .tooltip").hover(function () {
-    $(".tooltip").toggleClass("show");
-  });
-  //   $("#tooltipClose").click(function () {
-  //     $(".tooltip").hide();
-  //   });
+  //툴팁 close 버튼
 
-  $(".menu_list ul > li").click(function () {
+  $(".tip_icon").hover(function () {
+    $(this).children().toggleClass("show");
+  });
+  $(".tooltip_close").click(function () {
+    $(this).parent().removeClass("show");
+  });
+
+  //모달 이벤트
+  $("input[name='phone']").click(function (e) {
+    e.preventDefault();
+    $("#testModal").modal("show");
+
+    $("#modalY").click(function () {
+      $("#testModal").modal("hide");
+      $("input[name='phone']").prop("checked", true);
+    });
+    $("#modalN").click(function () {
+      $("#testModal").modal("hide");
+      $("input[name='phone']").prop("checked", false);
+    });
+    $("#modalClose").click(function () {
+      $("#testModal").modal("hide");
+    });
+  });
+
+  // 대메뉴 style 변경
+  $(".tab_list_box > li").click(function () {
+    var index = $(this).index();
+
     if ($(this).is(".active") == true) {
       $(this).removeClass("active");
     } else {
       $(this).addClass("active");
+      $(this).siblings().removeClass("active");
+      $(".tab_cont > div").css("display", "none");
+
+      $(".tab_cont > div").eq(index).css("display", "block");
     }
   });
 
-  $(".ck_tab_list_ul li").click(function () {
+  //소메뉴 style 변경
+  $(".ck_tab_list_ul > li").click(function () {
     if ($(this).is(".active") == true) {
       $(this).removeClass("active");
     } else {
@@ -27,41 +58,4 @@ $(function () {
       $(this).siblings().removeClass("active");
     }
   });
-
-  //   $(".menu_list ul > li").click(function () {
-  //     if ($(this).is(".active") == true) {
-  //       $(this).removeClass("active");
-  //     } else {
-  //       $(this).addClass("active");
-  //       $(this).siblings().removeClass("active");
-  //     }
-  //   });
 });
-// $(".tab_list_box > li").click(function () {
-//   if ($(this).is(".active") == true) {
-//     $(this).removeClass("active");
-//   } else {
-//     $(this).addClass("active");
-//     $(this).siblings().removeClass("active");
-//   }
-// });
-
-// $(".btn_toggle").click(function () {
-//   if ($(this).is(".on") == true) {
-//     $(this).removeClass("on");
-//   } else {
-//     $(this).addClass("on");
-//   }
-// });
-
-// $(".checkin_status").click(function () {
-//   if ($(this).is(".on") == true) {
-//     $(this).removeClass("on");
-//     $(this).find("img").remove();
-//   } else {
-//     $(this).addClass("on");
-//     $(this).prepend(
-//       '<img src="/resource/css/golf/images/icon_check02.png" alt="">'
-//     );
-//   }
-// });
